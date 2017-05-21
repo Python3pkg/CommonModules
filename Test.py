8cf8ef33cd9365f7b79ec99d0c8a332f07ba7e93
Python3pkg <raliclo@gmail.com>
2017-05-21 01:59:34 -0700

    Convert to Python3

--- Test.py
+++ Test.py
@@ -8,12 +8,12 @@ from CommonModules.Utilities import TimeElapsed
 CM = CommonModules
 
 def main():
-    print(TimeElapsed())
+    print((TimeElapsed()))
     test_array = np.array([[4,5,6]])
     #test_array = np.array([])
-    print(CM.DataStructureOperations.CombineMatricesRowWise(test_array, np.array([[1,2,3]])))
+    print((CM.DataStructureOperations.CombineMatricesRowWise(test_array, np.array([[1,2,3]]))))
     #CM.IO.ExportNpArray("./test.txt", test_array)
-    print(TimeElapsed(LastTime = True))
+    print((TimeElapsed(LastTime = True)))
     return
 
 if __name__ == "__main__":
